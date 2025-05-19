cc71495c9943afd8ede1cfa15b6ac1d8c13e4c93
hernandezraymondm <hernandezraymondm@gmail.com>
2025-05-19 23:14:30 +0800

    add comments

--- server/database/inMemoryDB.js
+++ server/database/inMemoryDB.js
@@ -1,3 +1,5 @@
 // In-memory database
+// In this demo, we will use an in-memory database to store user credentials.
+// In a real application, you would use a proper database like MongoDB, PostgreSQL, etc.
 export const users = [];
 export const credentials = [];
